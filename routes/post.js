214a3b2b6550eb73023a27ca5b5cb5a78266b34e
putrihsnh <putrihasanah30@mhs.mdp.ac.id>
2023-05-31 11:34:33 +0700

    update

--- routes/post.js
+++ routes/post.js
@@ -4,55 +4,94 @@ const res = require("express/lib/response")
 const router = express.Router()
 const Post = require('../models/Post')
 
+function result(succ, msg, details) {
+  if (details) {
+    return {
+      success: succ,
+      message: msg,
+      data: details,
+    };
+  } else {
+    return {
+      success: succ,
+      message: msg,
+    };
+  }
+}
+
 router.get('/', async (req, res) => {
     try {
-        const post = await Post.find()
-        res.json(post)
-
-    } catch (eror) {
-        res.json({
-            message: eror
-        })
-
+        const post = await Post.aggregate([
+            {
+                $lookup: {
+                    from: 'user',
+                    localField: 'user_id',
+                    foreignField: '_id',
+                    as: 'userData'
+                }
+            },
+            {
+                $set: {
+                    id: "$_id",
+                    username: { arrayElemAt: ['$userData.username', 0] },
+                    created_date: { $dateToString: { format: ' %d-%m-%Y %H:%M:%S', date: '$created_date', timezone: '+07:00' } },
+                    modified_date: { $dateToString: { format: '$d-%m-%Y %H:%M:%S', date: '$modified_date', timezone: '+07:00' } }
+                }
+            },
+            {
+                $project: {
+                    userData: 0,
+                    _id: 0
+                }
+            }
+        ]);
+        if (this.post.length > 0) {
+            res.status(200).json(result(1, 'Retrieve data success', post))
+        } else {
+            res.status(200).json(result(0, 'zero data!'))
+        }
+    } catch (error) {
+        res.status(500).json(result(0, error.message))
     }
 })
 router.post('/', async (req, res) => {
     const inputPost = new Post({
         content: req.body.content,
-        user_id: req.body.user_id,
-        username: req.body.username
+        user_id: req.body.user_id
     })
     try {
         const post = await inputPost.save()
-        res.json(post)
-    } catch (eror) {
-        res.json({
-            message: eror
-        })
-    }
-})
-router.put('/:postId', async (req, res) => {
-    const data = {
-        content: req.body.content,
-    }
-    try {
-        const post = await Post.updateOne({
-            _id: req.params.postId
-        }, data)
-        res.json(post)
+        res.status(200).json(result(1, 'Insert post success!'))
     } catch (error) {
-        res.json({
-            message: error
-        })
+        res.status(500).json(result(0, error.message))
     }
 })
 
+router.put('/:postId', async (req, res) => {
+    const data = {
+        id: req.body.id,
+        content: req.body.content,
+        modified_date: Date.now()
+    }
+    try {
+        const post = await Post.updateOne({
+            _id: data.id
+        }, data)
+        if (post.matchedCount > 0) {
+            res.status(200).json(result(1, 'upadate post success!'))
+        }
+    } catch (error) {
+        res.status(500).json(result(0, error.message))
+        }
+})
 router.delete('/:postId', async (req, res) => {
     try {
         const post = await Post.deleteOne({
             _id: req.params.postId
         })
-        res.json(post)
+        if (post.deletedCount > 0) {
+            
+        }
     } catch (error) {
         res.json({
             message: error
